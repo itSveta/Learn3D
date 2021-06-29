cfb7b4e11979d5319577154e4f6463b23244d6c5
myDanielM <godinseccoming@gmail.com>
2021-06-29 21:26:57 +0300

    add models

--- Практика/script/three.js
+++ Практика/script/three.js
@@ -1,4 +1,6 @@
-var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH, renderer, container;
+
+var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH, renderer, container, models;
+
 var Colors = {
 	red:0xf25346,
 	white:0xd8d0d1,
@@ -7,7 +9,6 @@ var Colors = {
 	pink:0xF5986E,
 	yellow:0xf4ce93,
 	blue:0x68c3c0,
-
 };
 var hemisphereLight, shadowLight;
 function createScene() {
@@ -40,6 +41,7 @@ function createScene() {
 	camera.position.x = 0;
 	camera.position.z = 200;
 	camera.position.y = 100;
+	camera.lookAt(scene.position);
 
 	// Ренедер
 	renderer = new THREE.WebGLRenderer({
@@ -109,103 +111,50 @@ function createLights() {
 	scene.add(hemisphereLight);
 	scene.add(shadowLight);
 }
-// First let's define a Sea object :
-Sea = function(){
-
-	// create the geometry (shape) of the cylinder;
-	// the parameters are:
-	// radius top, radius bottom, height, number of segments on the radius, number of segments vertically
-	var geom = new THREE.CylinderGeometry(600,600,800,40,10);
-
-	// rotate the geometry on the x axis
-	geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
-
-	// create the material
-	var mat = new THREE.MeshPhongMaterial({
-		color:Colors.blue,
-		transparent:true,
-		opacity:.6,
-		shading:THREE.FlatShading,
+function addModels() {
+	models.forEach(element => {
+		scene.add(element);
 	});
-
-	// To create an object in Three.js, we have to create a mesh
-	// which is a combination of a geometry and some material
-	this.mesh = new THREE.Mesh(geom, mat);
-
-	// Allow the sea to receive shadows
-	this.mesh.receiveShadow = true;
 }
-// Instantiate the sea and add it to the scene:
-var sea;
-function createSea(){
-	sea = new Sea();
 
-	// push it a little bit at the bottom of the scene
-	sea.mesh.position.y = -600;
-
-	// add the mesh of the sea to the scene
-	scene.add(sea.mesh);
-}
-Cloud = function(){
-	// Create an empty container that will hold the different parts of the cloud
-	this.mesh = new THREE.Object3D();
-
-	// create a cube geometry;
-	// this shape will be duplicated to create the cloud
-	var geom = new THREE.BoxGeometry(20,20,20);
-
-	// create a material; a simple white material will do the trick
-	var mat = new THREE.MeshPhongMaterial({
-		color:Colors.white,
-	});
-
-	// duplicate the geometry a random number of times
-	var nBlocs = 3+Math.floor(Math.random()*3);
-	for (var i=0; i<nBlocs; i++ ){
-
-		// create the mesh by cloning the geometry
-		var m = new THREE.Mesh(geom, mat);
-
-		// set the position and the rotation of each cube randomly
-		m.position.x = i*15;
-		m.position.y = Math.random()*10;
-		m.position.z = Math.random()*10;
-		m.rotation.z = Math.random()*Math.PI*2;
-		m.rotation.y = Math.random()*Math.PI*2;
-
-		// set the size of the cube randomly
-		var s = .1 + Math.random()*.9;
-		m.scale.set(s,s,s);
-
-		// allow each cube to cast and to receive shadows
-		m.castShadow = true;
-		m.receiveShadow = true;
-
-		// add the cube to the container we first created
-		this.mesh.add(m);
-	}
-}
 
 function init() {
+	
 	// настраиваем сцену, камеру и рендер
 	createScene();
 
 	// add the lights
 	createLights();
 
-	// add the objects
-	//createPlane();
-	createSea();
-	//createSky();
+	//add models
+	//addModels();
+
+	makeCube();
+	makeCube();
+	makeCube();
+	makeCube();
+
 	renderer.render(scene, camera);
-	// start a loop that will update the objects' positions
-	// and render the scene on each frame
-	//loop();
+	
 }
 
 
-window.onload = function() {
-
+window.onload = function () {
+	console.log(Math.random()*100)
 	init();
 }
+
+
+function makeCube() {
+	var sphereGeometry = new THREE.SphereGeometry(40, 20, 20);
+	var sphereMaterial = new THREE.MeshBasicMaterial(
+		{ color: 0x7777ff, wireframe: true });
+	var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	sphere.position.x = Math.random()*200;
+	sphere.position.y = Math.random()*200;
+	sphere.position.z = 2;
+	scene.add(sphere);
+};
+
+
 
